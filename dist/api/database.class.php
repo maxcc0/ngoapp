9f47ad93d824eb933f8161291e2fa340781a15a3
Jiten Poddar <Jiten.Poddar@ad.infosys.com>
2016-12-06 09:05:34 +0530

    update db connection string

--- dist/api/database.class.php
+++ dist/api/database.class.php
@@ -5,9 +5,9 @@ class Database
 				function connect_database()
 							{
 									$DB_host = "localhost";
-									$DB_user = "socialpixe";
-									$DB_pass = "socialpixe";
-									$DB_name = "socialpixe";
+									$DB_user = "domutthiapp";
+									$DB_pass = "domutthiapp";
+									$DB_name = "domutthiapp";
 									$conn = mysqli_connect($DB_host,$DB_user,$DB_pass,$DB_name);
 									
 									return $conn;
